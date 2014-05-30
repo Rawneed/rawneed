797a804cc389c518c3ec59a0b1553c35a2184321
Bode <bodefuwa@gmail.com>
2014-05-30 16:51:15 -0400

    more hopeful final deploy config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/rawneed_admin/var/www/rawneed/current"
+root = "/var/www/rawneed/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
