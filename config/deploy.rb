ab8c4355eef3f4494d527482a244374a4e13b494
Bode <bodefuwa@gmail.com>
2014-06-01 21:11:07 -0400

    trying new deploy.rb config 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,88 +1,22 @@
-set :application, 'rawneed'
-set :deploy_user, 'rawneed_admin'
-
-# setup repo details
-set :scm, :git
+set :application, ''rawneed
 set :repo_url, '/var/git/rawneed.git'
 
-# setup rvm.
-set :rbenv_type, :user
-set :rbenv_ruby, '2.1.2'
+set :deploy_to, '/var/www/#{fetch(:application}'
+set :deploy_user, 'rawneed_admin'
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, "2.1.2"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
-# how many old releases do we want to keep
-set :keep_releases, 5
-
-# files we want symlinking to specific entries in shared.
 set :linked_files, %w{config/database.yml}
-
-# dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# what specs should be run before deployment is allowed to
-# continue, see lib/capistrano/tasks/run_tests.cap
-# set :tests, []
-
-# which config files should be copied by deploy:setup_config
-# see documentation in lib/capistrano/tasks/setup_config.cap
-# for details of operations
-set(:config_files, %w(
-  nginx.conf
-  database.yml
-  unicorn.rb
-  unicorn_init.sh
-))
-
-# which config files should be made executable after copying
-# by deploy:setup_config
-set(:executable_config_files, %w(
-  unicorn_init.sh
-))
-
-# files which need to be symlinked to other parts of the
-# filesystem. For example nginx virtualhosts, log rotation
-# init scripts etc.
-set(:symlinks, [
-  {
-    source: "nginx.conf",
-    link: "/etc/nginx/sites-enabled/#{fetch(:application)}"
-  },
-  {
-    source: "unicorn_init.sh",
-    link: "/etc/init.d/unicorn_#{fetch(:application)}"
-  }
-])
-
-
-# this:
-# http://www.capistranorb.com/documentation/getting-started/flow/
-# is worth reading for a quick overview of what tasks are called
-# and when for `cap stage deploy`
+set :keep_releases, 5
 
 namespace :deploy do
-# make sure we're deploying what we think we're deploying
-before :deploy, "deploy:check_revision"
-# only allow a deploy with passing tests to deployed
-# before :deploy, "deploy:run_tests"
-# compile assets locally then rsync
-after 'setup:symlink_config', 'deploy:compile_assets_locally'
-after :finishing, 'deploy:cleanup'
-
-# remove the default nginx configuration as it will tend
-# to conflict with our configs.
- before 'deploy:setup_config', 'nginx:remove_default_vhost'
-
-# reload nginx to it will pick up any modified vhosts from
-# setup_config
- after 'deploy:setup_config', 'nginx:reload'
-
-# Restart monit so it will pick up any monit configurations
-# we've added
-# after 'deploy:setup_config', 'monit:restart'
-
-# As of Capistrano 3.1, the `deploy:restart` task is not called
-# automatically.
- after 'deploy:publishing', 'deploy:restart'
- after :rollback, 'deploy:restart'
+  before :deploy, "deploy:check_revision"
+  after :deploy, "deploy:restart"
+  after :rollback, "deploy:restart"
 end
