92356cad137d9baf6afbc7c1d871b8ea799c93ff
Bode <bodefuwa@gmail.com>
2014-06-01 21:20:55 -0400

    trying new deploy.rb config 8

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ set :repo_url, '/var/git/rawneed.git'
 set :deploy_to, '/var/www/#{fetch(:application)}'
 set :deploy_user, 'rawneed_admin'
 
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, "2.1.2"
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
