07dcecd16f54ed8af3f4f4fbc178eacdd5aa465b
Bode <bodefuwa@gmail.com>
2014-06-01 21:06:40 -0400

    trying new deploy.rb config 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,12 +66,12 @@ before :deploy, "deploy:check_revision"
 # only allow a deploy with passing tests to deployed
 # before :deploy, "deploy:run_tests"
 # compile assets locally then rsync
-after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+after 'setup:symlink_config', 'deploy:compile_assets_locally'
 after :finishing, 'deploy:cleanup'
 
 # remove the default nginx configuration as it will tend
 # to conflict with our configs.
-# before 'deploy:setup_config', 'nginx:remove_default_vhost'
+ before 'deploy:setup_config', 'nginx:remove_default_vhost'
 
 # reload nginx to it will pick up any modified vhosts from
 # setup_config
@@ -84,4 +84,5 @@ after :finishing, 'deploy:cleanup'
 # As of Capistrano 3.1, the `deploy:restart` task is not called
 # automatically.
  after 'deploy:publishing', 'deploy:restart'
+ after :rollback, 'deploy:restart'
 end
